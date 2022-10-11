0bd5f44934f4d071ae1130a43f658bd9d0f9b79c
Matteo Penna <penna.matteo@gmail.com>
2022-10-11 22:11:41 +0200

    Added systemd management

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -2,8 +2,9 @@ using Microsoft.AspNetCore.HttpOverrides;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Host.UseSystemd();
 
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
