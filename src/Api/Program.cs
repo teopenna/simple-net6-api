74601316881354d19e16930f15e0d383f71f864a
Matteo Penna <penna.matteo@gmail.com>
2023-01-03 21:37:56 +0100

    Adding authentication changes

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Test comment
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
